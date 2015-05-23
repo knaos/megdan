50779725a2cd39a2f0b41ba3c9ca71d7fa4cf658
knaos <akaynarov@gmail.com>
2015-05-23 23:53:19 +0300

    Fixed bugs. Added self evaluation. If I had only 4 more hours :S

--- Megdan/app/components/profile/controllers/FriendsController.js
+++ Megdan/app/components/profile/controllers/FriendsController.js
@@ -6,6 +6,10 @@ app.controller("FriendsController", function ($scope, profileService) {
             $scope.friends = data;
         }
     );
+    $scope.profileData = {};
+    profileService.getDataAboutMe(function (data) {
+        $scope.profileData = data;
+    });
 
     $scope.nameFilter = '';
 
